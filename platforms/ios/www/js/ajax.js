36bb3f82522dc5a90427c61f6ac092909965ac46
aezeylan <aez@MacBook-Pro-van-Emir.local>
2016-03-25 21:20:23 +0100

    te fixen werk
    Te fixen:
    
    BUGS:
    - grafiek + legenda layout 1 uur
    - settings save bug, gaat verkeerd bij analyse save door localstorage 1
    uur
    - list item click details 1 uur
    - endless scroll, niet echt slim, hij laadt van te voren alles al dan
    pas scroll. 2 uur
    
    Te beginnen:
    - 1 native function 4 uur
    - pages onthouden 1 uur
    - sprites images 1 uur

---
+++ platforms/ios/www/js/ajax.js
@@ -0,0 +1,287 @@
+/* check scroll function */
+function messagePost() {
+   $('#button_send').on('tap',function(event){
+    event.preventDefault();
+
+    var text = $('#text').val();
+    var regex = /[^\r\n.!?]+(:?(:?\r\n|[\r\n]|[.!?])+|$)/gi;
+    var split = text.match(regex).map($.trim);
+    var message = split.join("\\n");
+    var data = '{"text": "'+message+'"}';
+    // console.log(message);
+    // console.log(data);
+
+    // route url aangepast naar /analyzer/analyze
+    $.ajax({
+        beforeSend: function() { $.mobile.loading("show") }, 
+        complete: function() { $.mobile.loading("hide") },
+        url: "https://emotionanalyzer.herokuapp.com/analyzer",
+        type: "post",
+        contentType: "application/json",
+        data: data,
+        success: function (response) {
+           // you will get response from your php page (what you echo or print)                 
+           console.log(response);
+           getResults(response); 
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+           console.log(textStatus, errorThrown);
+        }
+    });
+  });
+}
+
+function filterToneScore(sort,array) {
+  var score = 0;
+  var tone = 0;
+  for (var key in array) {
+    if (array.hasOwnProperty(key)) {
+      if(array[key][sort] > score)
+      {
+          score = array[key][sort];
+          tone = array[key];
+      }
+    }
+  }
+  return tone;
+}
+
+function getResults(response) {
+      saveAnalyse(response);
+      var list = '<ul data-role="listview" id="list">';
+      var e_tone;
+      var w_tone;
+      var s_tone;
+      var tone_result;
+      var toneCategorie;
+      var sentences = response.analysis.sentences_tone;
+      
+      if(sentences){
+        for (var i = 0; i < sentences.length; i++) {
+            toneCategorie  = sentences[i].tone_categories; 
+            
+            for (var j = 0; j < toneCategorie.length; j++) {
+                if(j==0) {
+                  e_tone = toneCategorie[j].tones;
+                   for (var k = 0; k < e_tone.length; k++) {
+                    e_tone[k].score;
+                  }
+                }
+                if(j==1) {
+                  w_tone = toneCategorie[j].tones;
+                  for (var k = 0; k < w_tone.length; k++) {
+                    w_tone[k].score;
+                  }
+                }
+                if(j==2) {
+                  s_tone = toneCategorie[j].tones;
+                  for (var k = 0; k < s_tone.length; k++) {
+                    s_tone[k].score;
+                  }
+                }   
+            }
+
+              list += '<li>';
+              list += '<div class="sentence">'+sentences[i].text+'</div>';
+              list += '<div class="emotion-tone">'+filterToneScore("score",e_tone).tone_name+" - " +Math.round(filterToneScore("score",e_tone).score*100)+'%</div>';
+              list += '<div class="writing-tone">'+filterToneScore("score",w_tone).tone_name+" - " +Math.round(filterToneScore("score",w_tone).score*100)+'%</div>';
+              list += '<div class="social-tone">'+filterToneScore("score",s_tone).tone_name+" - " +Math.round(filterToneScore("score",s_tone).score*100)+'%</div>';
+              list += '</li>';
+        }
+        list += '</ul>';
+
+        $('<div id="resultAnalyse" class="panel"><div class="resultAnalyse-header"><a href="#" id="close_details" class="ui-btn ui-btn-inline ui-shadow ui-corner-all ui-btn-a ui-icon-delete ui-btn-icon-left">Close</a><h2>Analyse</h2></div><div class="graph-holder"><canvas id="graphA"></canvas><canvas id="graphB"></canvas><canvas id="graphC"></canvas></div><div class="list-holder">'+list+'</div></div>').insertBefore('#home');
+        $('#resultAnalyse').animate({'top':'0'},300);  
+        
+        var e_category = response.analysis.document_tone.tone_categories[0].tones;
+        var w_category = response.analysis.document_tone.tone_categories[1].tones;
+        var s_category = response.analysis.document_tone.tone_categories[2].tones;
+        createChart(e_category, w_category, s_category);
+        
+
+        $('#close_details').on('tap',function(){
+             $('#resultAnalyse').animate({'top':'-100%'},300, function(){$(this).remove();});  
+             $('#text').val('');
+        });
+    }else{
+        alert('An error has occured. Please try again! Try to write more than one English sentences.');
+    }
+}
+
+function createChart(e_category, w_category, s_category) {
+    //chartdata
+    var createJSONA;
+    var colorsA = [ "red", "purple", "green", "orange", "blue"];
+    var colorsB = [ "cyan", "blue", "gray"];
+    var colorsC = [ "red", "purple", "green", "orange", "blue"];
+
+    var dataA = [];
+
+    for (var i = 0; i < e_category.length; i++) {
+      createJSONA = {};
+      createJSONA["value"] = e_category[i].score*100;
+      createJSONA["color"] = colorsA[i];
+      createJSONA["highlight"] = colorsA[i];
+      createJSONA["label"] = e_category[i].tone_name;
+
+       dataA.push(createJSONA); 
+    }
+
+    var createJSONB;
+    var dataB = [];
+    for (var i = 0; i < w_category.length; i++) {
+
+      createJSONB = {};
+      createJSONB["value"] = w_category[i].score*100;
+      createJSONB["color"] = colorsB[i];
+      createJSONB["highlight"] = colorsB[i];
+      createJSONB["label"] = w_category[i].tone_name;
+
+       dataB.push(createJSONB); 
+    }
+
+
+    var createJSONC;
+    var dataC = [];
+
+    for (var i = 0; i < s_category.length; i++) {
+      createJSONC = {};
+      createJSONC["value"] = s_category[i].score*100;
+      createJSONC["color"] = colorsC[i];
+      createJSONC["highlight"] = colorsC[i];
+      createJSONC["label"] = s_category[i].tone_name;
+
+       dataC.push(createJSONC); 
+    }
+ 
+    var myDoughnutChartA = new Chart(document.getElementById("graphA").getContext("2d")).Doughnut(dataA);
+    var myDoughnutChartB = new Chart(document.getElementById("graphB").getContext("2d")).Doughnut(dataB);
+    var myDoughnutChartC = new Chart(document.getElementById("graphC").getContext("2d")).Doughnut(dataC);
+// var ctx = document.getElementById("graphA").getContext("2d");
+
+// var myChart = new Chart(ctx).Doughnut(dataA, options);
+    // document.getElementById('js-legend').innerHTML = myDoughnutChartA.generateLegend();
+
+
+  // var ctx = document.getElementById("graphA").getContext("2d");
+  // var pieChart = new Chart(ctx).Pie(dataA);
+
+  // legend(document.getElementById("graphA"), dataA, pieChart);
+
+}
+
+function saveAnalyse(data) {
+    var dataAnalyse = JSON.stringify(data);
+    window.localStorage.setItem(data._id, dataAnalyse);
+    // addhistory oproepen
+    addToHistory(data._id);
+}
+
+function showHistory() {
+  var list = '<ul data-role="listview" id="list">';
+  var sentences; 
+
+  list += '</ul>';
+  $('.history.list-holder').append(list);
+
+
+  for (var analyseItem in localStorage) {
+    addToHistory(analyseItem);
+  }
+
+
+  $(document).on('tap', '#show-details' ,function(analyse_id){ 
+    analyse_id = $(this).children('.analyse_id').attr('id');
+    createDetailsPage();
+    $.mobile.changePage('#details', { transition: "slide"} );
+    showHistoryDetails(analyse_id);
+  });
+}
+
+function addToHistory(analyse_id){
+  //jquery append id=list 
+  sentences = JSON.parse(localStorage[analyse_id]);
+  var li = '<li><a href="#" id="show-details">' +
+  '<div class="analyse_id" id="'+sentences._id+'">'+sentences.text.substring(0,40)+'...</div>' +
+  '<div class="sentence">'+sentences.text.substring(0,40)+'...</div>' +
+  '<div class="analyse-date">'+sentences.date.split('T')[0]+'</div></a></li>';
+  $('.history.list-holder #list').append(li);
+}
+
+//DIT IS EIGENLIJK DE DETAILS, CODE MOET VEEL SMALLER
+function showHistoryDetails(analyse_id) {
+
+    var localData = JSON.parse(window.localStorage.getItem(analyse_id));
+    var list = '<ul data-role="listview" id="list">';
+    var e_tone;
+    var w_tone;
+    var s_tone;
+    var tone_result;
+    var toneCategorie;
+    var sentences = localData.analysis.sentences_tone;
+
+    for (var i = 0; i < sentences.length; i++) {
+      toneCategorie  = sentences[i].tone_categories; 
+            
+        for (var j = 0; j < toneCategorie.length; j++) {
+            if(j==0) {
+              e_tone = toneCategorie[j].tones;
+                for (var k = 0; k < e_tone.length; k++) {
+                e_tone[k].score;
+              }
+            }
+            if(j==1) {
+              w_tone = toneCategorie[j].tones;
+              for (var k = 0; k < w_tone.length; k++) {
+                w_tone[k].score;
+              }
+            }
+            if(j==2) {
+              s_tone = toneCategorie[j].tones;
+              for (var k = 0; k < s_tone.length; k++) {
+                s_tone[k].score;
+              }
+            }   
+        }
+
+        list += '<li>';
+        list += '<div class="sentence">'+sentences[i].text+'</div>';
+        list += '<div class="emotion-tone">'+filterToneScore("score",e_tone).tone_name+" - " + Math.round(filterToneScore("score",e_tone).score*100)+'%</div>';
+        list += '<div class="writing-tone">'+filterToneScore("score",w_tone).tone_name+" - " +Math.round(filterToneScore("score",w_tone).score*100)+'%</div>';
+        list += '<div class="social-tone">'+filterToneScore("score",s_tone).tone_name+" - " +Math.round(filterToneScore("score",s_tone).score*100)+'%</div>';
+        list += '</li>';
+    }
+    list += '</ul>';
+
+    $('.details.list-holder').append(list);
+
+     var e_category = localData.analysis.document_tone.tone_categories[0].tones;
+        var w_category = localData.analysis.document_tone.tone_categories[1].tones;
+        var s_category = localData.analysis.document_tone.tone_categories[2].tones;
+        createChart(e_category, w_category, s_category);
+
+    $('#close_details').on('tap',function(){
+       $.mobile.changePage('#history', { transition: "slide", reverse: true} );
+       $('#details').remove();
+    });
+         
+}
+
+function createDetailsPage() {
+  var detailsPage = '<div data-role="page" id="details"><div data-role="header"><h1>Emotional Analyzer - Details</h1><a href="#" id="close_details" class="ui-btn ui-btn-inline ui-shadow ui-corner-all ui-btn-a ui-icon-delete ui-btn-icon-left">Close</a></div><div role="main" class="ui-content"><div class="details graph-holder"><canvas id="graphA"></canvas><div id="placeholder"></div><canvas id="graphB"></canvas><canvas id="graphC"></canvas></div><div class="details list-holder"></div></div><div data-role="footer"><h4>Footer Text</h4></div></div>';
+  $(detailsPage).insertAfter('#history');
+}
+
+function refreshData() {
+  $( "#history" ).on( "pagebeforeshow", function( event ) {
+      $('#list').listview('refresh');
+  }); 
+}
+
+
+$(document).on('ready', function(){
+    messagePost();
+    showHistory();
+    refreshData();
+});
+
--- www/js/custom.js
+++ www/js/custom.js
@@ -1,87 +1,23 @@
 $(document).delegate('.ui-page', "swipeleft", function(){
     var $nextPage = $(this).next('[data-role="page"]');
     // swipe using id of next page if exists
-    if ($nextPage.length > 0) {
+    if ($nextPage.length > 0 && $.mobile.activePage.attr('id') != 'details') {
         $.mobile.changePage($nextPage, { transition: 'slide' });
     }
 }).delegate('.ui-page', "swiperight", function(){
     var $prevPage = $(this).prev('[data-role="page"]');
     // swipe using id of next page if exists
-    if ($prevPage .length > 0) {
+    if ($prevPage .length > 0 && $.mobile.activePage.attr('id') != 'details') {
         $.mobile.changePage($prevPage, { transition: 'slide', reverse : true });
     }
 });
-
-
-(function( $, window, undefined ) {
-    $.widget( "mobile.listview", $.mobile.listview, {
-        options: {
-            childPages: true,
-            page: "<div data-role='page'></div>",
-            header: "<div data-role='header'><a href='#' data-rel='back'>Back</a><h1></h1></div>",
-            content: "<div class='ui-content'></div>"
-        },
-        _create: function(){
-            this._super();
-            if( this.options.childPages ) {
-                this._setupChildren();
-            }
-        },
-        _setupChildren: function() {
-            this._attachBindings();
-            this.element.find( "ul" )
-                .css( "display","none" )
-                .parent()
-                .addClass("ui-btn ui-btn-icon-right ui-icon-carat-r");
-        },
-        _attachBindings: function() {
-            this._on({
-                "click": "_handleSubpageClick"
-            });
-            this._on( "body", {
-                "pagechange": function(){
-                    if ( this.opening === true ) {
-                        this.open = true;
-                        this.opening = false;
-                    } else if ( this.open === true ) {
-                        this.newPage.remove();
-                        this.open = false;
-                    }
-                }
-            });
-        },
-        _handleSubpageClick: function( event ) {
-            if( $(event.target).closest( "li" ).children( "ul" ).length == 0 ) {
-                return;
-            }
-            this.opening = true;
-            this.newPage = $( this.options.page ).uniqueId();
-            this.nestedList  = $( event.target ).children( "ul" )
-                .clone().attr( "data-" + $.mobile.ns + "role", "listview" )
-                .css( "display", "block" );
-            this.pageName = (
-                $( event.target.childNodes[0] ).text().replace(/^\s+|\s+$/g, '').length > 0 )?
-                $( event.target.childNodes[0] ).text() : $( event.target.childNodes[1] ).text();
-            this.pageID = this.newPage.attr( "id" );
-            // Build new page
-            this.newPage.append(
-                $( this.options.header ).find( "h1" ).text( this.pageName ).end()
-            ).append(
-                $( this.options.content )
-            ).find( "div.ui-content" ).append( this.nestedList );
-            $( "body" ).append( this.newPage );
-            $( "body" ).pagecontainer( "change", "#" + this.pageID );
-        }
-    });
-})( jQuery, this );
-
 
 
 
 /* for demo only */
 $(document).on("pagebeforecreate", "#history", function(e, ui) {
   var items = '';
-  for (var i = 1; i < 100; i++) {
+  for (var i = 1; i < 40; i++) {
     items += "<li>" + i + "</li>";
   }
   $("#list").append(items);
@@ -95,7 +31,7 @@ function checkScroll() {
     header = $(".ui-header", activePage).outerHeight() - 1,
     scrolled = $(window).scrollTop(),
     footer = $(".ui-footer", activePage).outerHeight() - 1,
-    scrollEnd = contentHeight - screenHeight + header + footer;
+    scrollEnd = contentHeight - screenHeight + header + footer;;
   if (activePage[0].id == "history" && scrolled >= scrollEnd) {
     console.log("adding...");
     addMore(activePage);
@@ -114,7 +50,7 @@ function addMore(page) {
       last = $("li", page).length,
       cont = last + 5;
     for (var i = last; i < cont; i++) {
-      items += "<li>" + i + "</li>";
+      items += "<li>Demo</li>";
     }
     $("#list", page).append(items).listview("refresh");
     $.mobile.loading("hide");
